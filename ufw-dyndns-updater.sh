df9809848077af12b7a5cbcd6fc1062bd7b29bcd
TF <18724325+se-di@users.noreply.github.com>
2022-10-31 09:09:01 +0100

    ufw-ddu: print on errors only

--- ufw-dyndns-updater.sh
+++ ufw-dyndns-updater.sh
@@ -7,7 +7,7 @@
 #   # re-create dyndns based UFW rule 
 #   */5 * * * * root /root/ufw-dyndns-updater.sh
 ##########################################################################
-VERSION="1.0"
+VERSION="1.1"
 DEBUG=0
 ##############
 
@@ -28,11 +28,11 @@ if [ "$new_ip" = "$old_ip" ] ; then
     true
 else
     if [ -n "$old_ip" ] ; then
-        /usr/sbin/ufw delete allow from $old_ip to any port $PORT proto $PROTO
+        /usr/sbin/ufw delete allow from $old_ip to any port $PORT proto $PROTO 1> /dev/null
     fi
     if [ ! -z "$new_ip" ];then
         [ $DEBUG -eq 1 ] && echo iptables will be updated
-        /usr/sbin/ufw allow from $new_ip to any port $PORT proto $PROTO comment $HOSTNAME 
+        /usr/sbin/ufw allow from $new_ip to any port $PORT proto $PROTO comment $HOSTNAME 1> /dev/null
     else
         echo "new_ip empty.. not updating ufw - BUT old_ip entry has been removed - so no rule active atm.."
     fi
